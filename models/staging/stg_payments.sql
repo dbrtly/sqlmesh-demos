1ad6fada1b845be2401c50b6b449d2acdf3083f9
Sung Won Chung <sungwonchung3@gmail.com>
2024-05-14 15:28:26 -0700

    Clean up for future demos (#4)
    * add a simple test generation script
    
    * clean up demo model
    
    * remove old test

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -14,6 +14,9 @@ SELECT
   order_id,
   payment_method,
   amount / 100 AS amount, /* `amount` is currently stored in cents, so we convert it to dollars */
-  'new_column' AS new_column, /* non-breaking change example  */
-  'another_column' AS another_column
+  -- 'new_column' AS new_column, /* non-breaking change example  */
 FROM demo.seed_raw_payments
+
+-- how to generate unit test code without manually writing yaml by hand
+-- this will generate a file in the tests/ folder: test_stg_payments.yaml
+-- sqlmesh create_test demo.stg_payments --query demo.seed_raw_payments "select * from demo.seed_raw_payments limit 5" 
